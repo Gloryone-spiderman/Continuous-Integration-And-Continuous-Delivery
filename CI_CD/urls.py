c6e52cceef3214861f1d359fd9b0c1169d288fd8
michone <hyz2370628917@outlook.com>
2025-09-29 17:29:26 +0800

    上述修改配置没有上传完，进行补充一下

--- CI_CD/urls.py
+++ CI_CD/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apps.api.urls')),
+    path('api/', include('api.urls')),
 ]
