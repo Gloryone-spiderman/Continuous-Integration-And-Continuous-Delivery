bac4ec4175152b09a5979377cd47d56b51738c43
michone <hyz2370628917@outlook.com>
2025-09-29 14:52:47 +0800

    修改了启动shell调试无法import模型的问题，修改了项目应用的sys.path以及appconfig内容。

--- CI_CD/settings.py
+++ CI_CD/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 此后添加应用
     'rest_framework',
-    'apps.api',
+    'api',
 ]
 
 MIDDLEWARE = [
--- apps/api/models.py
+++ apps/api/models.py
@@ -6,8 +6,14 @@ class Question(models.Model):
     name = models.CharField(max_length=100)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.name
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
